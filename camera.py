5981b18338f0009b1b18edaf8d052f991187418e
StoneInHisWorld <1195290789@qq.com>
2024-04-16 17:35:25 +0800

    try to integrate asynchronously shoot, but fail.

--- camera.py
+++ camera.py
@@ -8,18 +8,22 @@ class Camera:
         初始化Vimba相机
         :param no: 取当前连接的第no号相机
         """
+        self.__no = no
+        self.__exposTime = 50
+        self.__gain = 0
+        self.__close = False
         with Vimba.get_instance() as vimba:
             cams = vimba.get_all_cameras()
             assert len(cams) >= 1, 'No camera detected!'
             self.__device = cams[no]
 
-    def set_expostime(self, expostime):
+    def set_exposTime(self, exposTime):
         """
         设置相机曝光时间
-        :param expostime: 曝光时间
+        :param exposTime: 曝光时间
         :return: None
         """
-        self.__device.ExposureTimeAbs = expostime
+        self.__exposTime = exposTime
 
     def set_gain(self, gain):
         """
@@ -27,18 +31,31 @@ class Camera:
         :param gain: 增益
         :return: None
         """
-        self.__device.Gain = gain
+        self.__gain = gain
 
-    def shoot(self, px_format=PixelFormat.Mono8) -> Frame:
+    def shoot(self, px_format=PixelFormat.Mono8, timeout=2000) -> Frame:
         """
         同步单帧拍照
-        :param px_format: 帧像素格式，一般设置为
+        :param px_format: 帧像素格式，一般设置为PixelFormat.Mono8
+        :param timeout: 拍照超时时间，超过时间则判定为相机出错。
         :return: 拍照帧
         """
-        frame = self.__device.get_frame()
-        frame.convert_pixel_format(px_format)
-        return frame
+        self.__close = False
+        with self.__device as cam:
+            while not self.__close:
+                self.__update_param(cam)
+                frame = cam.get_frame(timeout)
+                frame.convert_pixel_format(px_format)
+                yield frame
 
+    def __update_param(self, cam):
+        cam_expostime = cam.ExposureTimeAbs
+        cam_gain = cam.Gain
+        cam_expostime.set(self.__exposTime)
+        cam_gain.set(self.__gain)
+
+    def close(self):
+        self.__close = True
 
 
 # Synchronous grab
